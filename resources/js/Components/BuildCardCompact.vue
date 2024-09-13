d4050153198a74d085e313c5a9a496177bdc1425
Miho Daniel <miho.daniel@mdy.hu>
2024-09-13 22:17:49 +0200

    style compact build card to match regular

--- resources/js/Components/BuildCardCompact.vue
+++ resources/js/Components/BuildCardCompact.vue
@@ -12,15 +12,22 @@ const props = defineProps({
 <template>
     <Link
         :href="route('build.show', build)"
-        class="block relative bg-zinc-800 rounded-xl p-4 hover:ring-2 hover:ring-blue-500"
+        class="block relative bg-zinc-900 rounded-xl p-4 ring-1 ring-white/10 hover:ring-2 hover:ring-blue-500 overflow-hidden"
     >
-        <p class="font-bold text-sm truncate">{{ build.name }}</p>
+        <img
+            :src="`/img/${build.diablo_class.name.toLowerCase()}.png`"
+            :alt="build.diablo_class.name"
+            class="absolute top-0 right-0 opacity-40 transform transition-all ease-in-out group-hover:opacity-70 group-hover:scale-105"
+        />
 
-        <div class="flex items-center justify-between">
-            <p class="text-zinc-500 font-semibold text-xs">
+        <div class="relative">
+            <p class="font-bold text-sm truncate mb-1">
+                {{ build.name }}
+            </p>
+
+            <p class="text-white/50 font-semibold text-xs">
                 {{ build.diablo_class.name }}
             </p>
-            <p class="text-zinc-500 text-xs">By {{ build.author.name }}</p>
         </div>
     </Link>
 </template>

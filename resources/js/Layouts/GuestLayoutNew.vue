abc3596b4237709ee090eb13bd0ca08fd2cee3cb
Miho Daniel <miho.daniel@mdy.hu>
2024-09-13 22:20:23 +0200

    make guest layout one column

--- resources/js/Layouts/GuestLayoutNew.vue
+++ resources/js/Layouts/GuestLayoutNew.vue
@@ -3,15 +3,11 @@ import { IconMicrowaveFilled } from "@tabler/icons-vue";
 </script>
 
 <template>
-    <div
-        class="min-h-svh grid grid-cols-2 items-center pt-6 sm:pt-0 bg-zinc-900"
-    >
-        <div class="bg-orange-900 h-full"></div>
-
-        <div class="w-full max-w-sm mx-auto px-6 text-zinc-400">
-            <div class="flex items-center space-x-6 mb-12">
+    <div class="min-h-svh pt-6 sm:pt-20 bg-zinc-900">
+        <div class="text-zinc-400 max-w-sm mx-auto">
+            <div class="flex items-center justify-center space-x-6 mb-12">
                 <div
-                    class="size-12 bg-gradient-to-r from-zinc-800 to-orange-800 rounded-full flex justify-center items-center"
+                    class="-ml-12 size-12 bg-gradient-to-r from-zinc-800 to-orange-800 rounded-full flex justify-center items-center"
                 >
                     <IconMicrowaveFilled
                         stroke-width="2"
